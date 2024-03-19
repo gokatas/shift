587337104ec3bfea1d1d12eff8c31ce65364b13f
jozef <jozef.reisinger@gmail.com>
2024-03-19 17:46:50 +0100

    simplify

--- README.md
+++
@@ -1,3 +0,0 @@
-Simple shift cipher. It encrypts a plaintext message by adding a key to each byte. It decrypts a ciphertext message by substracting a key from each byte.
-
-Adapted from https://github.com/bitfield/eg-crypto.
--- shift.go
+++ shift.go
@@ -1,5 +1,6 @@
-// Level: intermediate
-// Topics: design, crypto, testing
+// Package shift implemetns shift cipher. It encrypts a plaintext message by adding
+// a key to each byte. It decrypts a ciphertext message by substracting a key from
+// each byte. Adapted from https://github.com/bitfield/eg-crypto.
 package shift
 
 func Encrypt(plaintext []byte, key byte) []byte {
