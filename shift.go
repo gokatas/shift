b088a67d0098991dfa7a25635276e740cb25680e
jozef <jozef.reisinger@gmail.com>
2024-03-20 08:19:38 +0100

    fix typos

--- shift.go
+++ shift.go
@@ -1,5 +1,5 @@
-// Package shift implemetns shift cipher. It encrypts a plaintext message by adding
-// a key to each byte. It decrypts a ciphertext message by substracting a key from
+// Package shift implements shift cipher. It encrypts a plaintext message by adding
+// a key to each byte. It decrypts a ciphertext message by subtracting a key from
 // each byte. Adapted from https://github.com/bitfield/eg-crypto.
 package shift
 
